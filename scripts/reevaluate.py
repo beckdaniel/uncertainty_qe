8074bc1d06a952936e90307d5cc74a0b814802d1
beckdaniel <daniel.e.beck@gmail.com>
2016-03-14 14:53:49 +0000

    eval for amae

---
+++ scripts/reevaluate.py
@@ -0,0 +1,50 @@
+import numpy as np
+import GPy
+import util
+import sys
+import os
+from sklearn import cross_validation, preprocessing
+import gc
+
+
+def eval_and_report(model_name, kernel, warp, ard):
+    dataset_dir = os.path.join(MODEL_DIR, DATASET)
+    for fold in xrange(10):
+        fold_dir = os.path.join(SPLIT_DIR, DATASET, str(fold))
+        train_data = np.loadtxt(os.path.join(fold_dir, 'train'))
+        test_data = np.loadtxt(os.path.join(fold_dir, 'test'))
+        model_dir = os.path.join(dataset_dir, str(fold))
+        params_file = os.path.join(model_dir, 'params')
+
+        gp = util.train_gp_model(train_data, kernel, warp, ard, 
+                                 params_file=params_file, preload=True)
+        #metrics = util.get_metrics(gp, test_data)
+        asym_metrics = util.get_asym_metrics(gp, test_data)
+        util.save_asym_metrics(asym_metrics, os.path.join(model_dir, 'asym_metrics'))
+        gc.collect(2) # buggy GPy has allocation cycles...
+                
+    
+###########################    
+
+DATASET = sys.argv[1]
+KERNEL = sys.argv[2]
+WARP = sys.argv[3]
+ARD = sys.argv[4]
+MODEL_NAME = '_'.join([KERNEL, WARP, ARD])
+
+if ARD == 'False':
+    ARD = False
+else:
+    ARD = True
+if WARP == "None":
+    WARP = None
+
+SPLIT_DIR = os.path.join('..','splits')
+
+# Generate the splits. Each split has mean-normalized features and
+# pe-time per word in target segment.
+if KERNEL == 'split':
+    split_all_data()
+else:
+    MODEL_DIR = os.path.join('..', 'models', MODEL_NAME)
+    eval_and_report(MODEL_NAME, KERNEL, WARP, ARD)
--- scripts/util.py
+++ scripts/util.py
@@ -128,7 +128,37 @@ def get_metrics(model, test_data, median=False):
     rmse = np.sqrt(MSE(preds_mean, gold_labels))
     prs = pearson(preds_mean, gold_labels)
     nlpd = - np.mean(model.log_predictive_density(feats, gold_labels[:, None]))
+    pred_q = model.predict_quantiles(feats, quantiles=(25., 75.))[1].flatten()
     return mae, rmse, prs, nlpd
+
+
+def get_asym_metrics(model, test_data):
+    feats = test_data[:, :-1]
+    gold_labels = test_data[:, -1]
+    pess_maes = []
+    opt_maes = []
+    for i in xrange(1, 10):
+        q = (i / (1. + i)) * 100
+        preds_q_low, preds_q_high = model.predict_quantiles(feats, quantiles=(100 - q, q))
+        pess_mae = asym_mae(preds_q_high.flatten(), gold_labels, w=i)
+        opt_mae = asym_mae(preds_q_low.flatten(), gold_labels, w=i, optimistic=True)
+        pess_maes.append(pess_mae)
+        opt_maes.append(opt_mae)
+    return [pess_maes, opt_maes]
+
+
+def asym_mae(preds, gold, w=3, optimistic=False):
+    diffs = preds - gold
+    if optimistic:
+        zeros = diffs > 0
+    else:
+        zeros = diffs < 0
+    weights = (zeros * (w-1)) + 1 #hacky
+    return np.mean(np.abs(diffs) * weights)
+
+
+def save_asym_metrics(metrics, target):
+    np.savetxt(target, np.array(metrics).T, fmt='%.4f')
 
 
 def save_parameters(gp, target):
@@ -217,6 +247,72 @@ def load_parameters(gp, target):
             gp[p] = pdict[p]
 
 
+def dloss(ai, yi, yk):
+    n = yk.shape[0]
+    dot_ai = np.dot(ai.T, ai)
+    sum_ai = np.sum(ai)
+    dot_aiyi = np.dot(ai, yi)
+    sum_yi = np.sum(yi, axis=0)
+
+    num1 = n * dot_ai * yk
+    num2 = (sum_ai ** 2) * yk
+    num3 = sum_ai * dot_aiyi
+    num4 = dot_ai * sum_yi
+    num5 = dot_ai * yk
+    num = num1 - num2 + num3 - num4 - num5
+    
+    denom1 = n * dot_aiyi
+    denom2 = sum_ai * sum_yi
+    denom3 = dot_aiyi
+    denom = denom1 - denom2 - denom3
+
+    return np.sum(num / denom)
+
+
+def norm_a(a, mean):
+    mu, sigma = (a.mean(), a.std())
+    new_a = (a - mu) / sigma
+    mu_y, sigma_y = (mean.mean(), mean.std())
+    new_a *= sigma_y
+    new_a += mu_y
+    return new_a
+
+
+def prs_loss(mean, cov, samples=1000, its=10):
+    curr_a = np.copy(mean) # start with mean
+    #curr_a = np.ones_like(mean) + np.random.random(size=(SIZE))
+
+    n = curr_a.shape[0]
+    initial_a = np.copy(curr_a)
+    curr_a = norm_a(curr_a, mean)
+    for evals in xrange(its):
+        mv_samples = np.random.multivariate_normal(mean, cov, samples)
+        print pearson(mean, curr_a)
+        for i in xrange(n):
+            mask = np.ones(curr_a.shape, dtype=bool)
+            mask[i] = 0
+            ai = curr_a[mask]
+            yi = mv_samples.T[mask]
+            yk = mv_samples.T[i]
+            ak = dloss(ai, yi, yk)
+            curr_a[i] = ak / samples
+            #curr_a[i] = ak
+        #curr_a = norm_a(curr_a, mean)
+        print curr_a
+        print np.mean(np.abs(initial_a - curr_a))
+    return curr_a
+
+
+def get_pearson(gp, test_data, samples=1000, its=10):
+    feats = test_data[:, :-1]
+    gold_labels = test_data[:, -1]
+    mean, cov = gp.predict(feats, full_cov=True)
+    mean = mean.flatten()
+    prs_preds = prs_loss(mean, cov, samples=samples, its=its)
+    r_mean = pearson(mean, gold_labels)
+    r_loss = pearson(prs_preds.flatten(), gold_labels)
+    return r_mean, r_loss
+
 
 if __name__ == "__main__":
     import sys
@@ -242,14 +338,19 @@ if __name__ == "__main__":
 
     gp = train_gp_model(train_data, kernel, warp, ard)
     print gp
+    #print get_pearson(gp, test_data)
     #import ipdb; ipdb.set_trace()
     mae, rmse, ps, nlpd = get_metrics(gp, test_data)
-    #import ipdb; ipdb.set_trace()
+    amaes = get_asym_metrics(gp, test_data)
+    import pprint; pprint.pprint(amaes)
+    import ipdb; ipdb.set_trace()
     print "MAE:\t\t%.4f" % mae
+    #print "AMAE(3)(50):\t%.4f" % a_mae
+    #print "AMAE(3)(75):\t%.4f" % a_mae_75
     print "RMSE:\t\t%.4f" % rmse
     print "Pearsons:\t%.4f\t%.4f" % ps
     print "NLPD:\t\t%.4f" % nlpd
-    save_cautious_curves(gp, test_data, 'test_curves_none_alt')
-    save_predictions(gp, test_data, 'test_preds_none_alt')
+    #save_cautious_curves(gp, test_data, 'test_curves_none_alt')
+    #save_predictions(gp, test_data, 'test_preds_none_alt')
     #save_parameters(gp, '../saved_models/' + model)
     
